a5f02c9f7bc6397242eab5aa50e82b6e0d5ab95b
Sj1218305 <46301590+Sj1218305@users.noreply.github.com>
2019-05-01 10:11:59 +0530

    initial commit
    final project

---
+++ assets/JS/To-do list.js
@@ -0,0 +1,17 @@
+$("ul").on("click","li",function(){
+$(this).toggleClass("completed");
+});
+$("ul ").on("click","span",function(event){
+$(this).parent().fadeOut(500,function(){
+$(this).remove();
+});
+event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var value = $(this).val();
+        $("ul").append("<li>" +"<span><i class='fa  fa-trash'></i></span>"+ value + "</li>");
+    }
+});
+
+
